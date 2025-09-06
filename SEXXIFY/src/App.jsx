457cb3459174fef026e7ca72c86fd90a7652635a
Yazz <itsjustmaitai@gmail.com>
2025-09-06 11:41:25 +0300

    Created the landing page and styled it

---
+++ SEXXIFY/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Landingpage from './pages/Landingpage'
+
+function App() {
+ 
+  return (
+    <>
+      <div> <Landingpage/> </div>
+        </>
+  )}
+export default App
---
+++ SEXXIFY/src/pages/Landingpage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../css/LandingPage.css'
+import sofa from '../assets/sofa.png'
+
+
+function Landingpage() {
+  return (
+ <div className="landing-container">
+      {/* Navbar */}
+      <header className="navbar">
+        <nav>
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+        </nav>
+        <button className="book-btn">Book Now</button>
+      </header>
+
+      {/* Hero Section */}
+      <main className="hero">
+        <div className="side-text left">. . YOU WILL . .</div>
+        <div className="side-text right">. . LOVE THIS . .</div>
+
+        <h1 className="title">SEXXIFY</h1>
+
+        <img src={sofa} alt="Red Sofa" className="sofa" />
+      </main>
+    </div>
+  );
+}
+  
+
+export default Landingpage
